064a45723eadb8d68e6b4eb8cd60e5630a92b480
crspac <lauchnoob@gmail.com>
2019-05-17 14:48:33 +0200

    added example for textinput

--- app/src/main/java/com/example/gehaltsrechner/Fragment_Home.java
+++ app/src/main/java/com/example/gehaltsrechner/Fragment_Home.java
@@ -1,4 +1,42 @@
 package com.example.gehaltsrechner;
 
-public class Fragment_Home {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Fragment_Home extends Fragment {
+    private static final String TAG = "Fragment_Home";
+
+    private Button btnCalculate;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        btnCalculate = (Button) view.findViewById(R.id.btn_Calculate);
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Fragment1" , Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+
+    public Button getBtnCalculate() {
+        return btnCalculate;
+    }
+
+    public void setBtnCalculate(Button btnCalculate) {
+        this.btnCalculate = btnCalculate;
+    }
 }
--- app/src/main/java/com/example/gehaltsrechner/MainActivity.java
+++ app/src/main/java/com/example/gehaltsrechner/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.gehaltsrechner;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SectionStatePageAdapter mSectionStatePageAdapter;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSectionStatePageAdapter = new SectionStatePageAdapter(getSupportFragmentManager());
+
+        mViewPager = (ViewPager) findViewById(R.id.container);
+
+        setupViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        SectionStatePageAdapter adapter = new SectionStatePageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Fragment_Home(), "Home");
+        viewPager.setAdapter(adapter);
+    }
+
+    public void setViewPager(int fragmentNumber){
+        mViewPager.setCurrentItem(fragmentNumber);
     }
 }
