8350ba143f78e933c907a87fca75c83d25920cee
NovaCan <Ramon.h1239@googlemail.com>
2019-05-20 14:56:05 +0200

    Dropdownmenues

--- app/src/main/java/com/example/gehaltsrechner/FragmentHome.java
+++ app/src/main/java/com/example/gehaltsrechner/FragmentHome.java
@@ -7,10 +7,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-public class FragmentHome extends Fragment {
+public class FragmentHome extends Fragment implements AdapterView.OnItemSelectedListener {
     private static final String TAG = "Fragment_Home";
 
     private Button btnCalculate;
@@ -20,17 +23,42 @@ public class FragmentHome extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+
         //btnCalculate = (Button) view.findViewById(R.id.btn_Calculate);
 
-//        btnCalculate.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText(getActivity(), "FragmentHome" , Toast.LENGTH_SHORT).show();
-//            }
-//        });
+    //    btnCalculate.setOnClickListener(new View.OnClickListener() {
+    //        @Override
+    //
+    //        public void onClick(View v) {
+    //           Toast.makeText(getActivity(), "FragmentHome" , Toast.LENGTH_SHORT).show();
+    //       }
+    //   });
+
+        Spinner spinner = (Spinner) view.findViewById(R.id.dropdownfreibetrag);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.Kinderfreibetrag, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(this);
+
+        Spinner spinner1 = (Spinner) view.findViewById(R.id.dropdownsklasse);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(getContext(), R.array.Steuerklasse, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(this);
+
+        Spinner spinner2 = (Spinner) view.findViewById(R.id.dropdownarbeitsstelle);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(getContext(), R.array.Arbeitsstelle, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(this);
 
         return view;
     }
+
+
 
     public Button getBtnCalculate() {
         return btnCalculate;
@@ -38,5 +66,17 @@ public class FragmentHome extends Fragment {
 
     public void setBtnCalculate(Button btnCalculate) {
         this.btnCalculate = btnCalculate;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
